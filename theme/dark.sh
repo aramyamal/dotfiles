6bba1b45699fb9089bbc0bc1a2265fef08c67a52
aramyamal <aram@aramjamal.com>
2025-10-19 20:35:27 +0200

    more updates bby

--- theme/dark.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-THEME="base16-black-metal"
-
-for SOCKET in /run/user/1000/nvim.*; do
-  nvim --server "$SOCKET" --remote-send ":colorscheme $THEME<CR>"
-done
-
-kitty +kitten themes --reload-in=all 'Black Metal'
-hyprctl hyprpaper reload "eDP-1, ~/pictures/black_metal.jpg"
--- theme/light.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-THEME="everforest"
-
-for SOCKET in /run/user/1000/nvim.*; do
-  nvim --server "$SOCKET" --remote-send ":colorscheme $THEME<CR>"
-done
-
-kitty +kitten themes --reload-in=all 'Everforest Light Medium'
-hyprctl hyprpaper reload "eDP-1, ~/pictures/franz_richard_unterberger-amalfi.jpg"
